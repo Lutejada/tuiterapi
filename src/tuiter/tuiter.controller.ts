2530e63351e8898480c113de4eb47a1da012c993
Luis Tejada <luetejava@gmail.com>
2022-06-12 14:23:57 -0500

    se actualizo controlador

--- src/tuiter/tuiter.controller.ts
+++ src/tuiter/tuiter.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('tuiter')
-export class TuiterController {}
+export class TuiterController {
+  @Get()
+  getTuiters(): string {
+    return 'esto es un tuit';
+  }
+  @Get(':id')
+  getTuit(@Param('id') id: string): string {
+    return `esto es el id por parametro ${id}`;
+  }
+
+  @Post()
+  createtuit(@Body('message') message:string) {
+    return `El mensaje del Tuie es: ${message}`;
+  }
+}
